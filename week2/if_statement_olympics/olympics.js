5b93155acb4578e68ba5ec6e333d7da752a05858
jesse hernandez <mrjesse1283>
2020-10-30 23:29:27 -0600

    olympics-redo

--- week2/if_statement_olympics/olympics.js
+++ week2/if_statement_olympics/olympics.js
@@ -1,10 +1,14 @@
 // preliminary
+//#1
 if (5 > 3){
     console.log("is greater than");
 }
-if (cat = 3) {
+//#2
+//if (cat = 3)
+var length = "cat"; {
     console.log("is the length");
 }
+//#3
 cat = 2;
 dog = 5;
 if (dog == cat) {
@@ -17,17 +21,28 @@ if (dog == cat) {
      name: "Bobby",
      age: 12
 }
+//#1 
  if (person.age < 18){
      console.log("unable to go to the movies");
  } else {
      console.log("able to go to the movies");
  }
-if(person.name.startsWith("B"));{
+ //#2 
+ if ( person.name.charAt(0) === "B" ){
+    console.log("allowed");
+}   else
+    console.log("not allowed");
+/* if(person.name.startsWith("B"));{
     console.log("allowed with first letter");
-}
-if(person.name.startsWith("B") && person.age >= 18);{
+} */
+//#3
+if ( person.name.charAt(0) === "b" && person.age === 18){
+    console.log("allowed")
+} else 
+    console.log("not allowed");
+/* if(person.name.startsWith("B") && person.age >= 18);{
     console.log("allowed if both");
-}
+} */
 //Silver
 if ( 1 === 1 )
     console.log("strict");
