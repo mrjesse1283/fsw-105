6494a9c0f6c4fb0c9bcea71788b15d1f50d69b35
jesse hernandez <mrjesse1283>
2020-11-07 14:25:33 -0700

    nesting

---
+++ week3/nesting.js
@@ -0,0 +1,112 @@
+/* var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer", "computer" ]
+var count       = 0;
+
+for (var i = 0; i < officeItems.length; i++ ){
+   if (officeItems[i] === "computer"){
+    count++
+   }
+}
+console.log(count); */
+
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+    {   name: "Mike",
+        age: 12,
+        gender: "male"  }, 
+
+    {   name: "Madeline",
+        age: 80,
+        gender: "female"}, 
+
+    {   name: "Cheryl",
+        age: 22,
+        gender: "female"},
+
+    {   name: "Sam",
+        age: 30,
+        gender: "male"  },
+        
+    {   name: "Suzy",
+        age: 4,
+        gender: "female"}
+];
+//Number 1
+ //age old enough
+ function isOldEnough(){
+   for ( var i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++ ) {
+    if (peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18){
+        console.log("Old enough");
+    } else {
+        console.log("Not old enough");
+    }
+  }
+ }
+ isOldEnough();
+console.log("===========");
+
+//Number 2
+//name and age
+function isOldEnoughName(){
+for ( var i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++ ) {
+    if (peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18) {
+        console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is Old Enough to watch Mad Max.");
+    } else {
+        console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is Not Old Enough to Watch Mad Max.");
+    }
+}
+}
+isOldEnoughName();
+console.log("===========");
+
+//Number 3
+function nameAgeGender(){
+for ( var i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++ ) {
+    if (peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18) {
+        if(peopleWhoWantToSeeMadMaxFuryRoad[i].gender === "female"){
+        console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is Old Enough, let Her in.");
+    } else {
+        console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is old enough let HIM in.");
+    } 
+    } else {
+        if(peopleWhoWantToSeeMadMaxFuryRoad[i].gender === "female"){
+            console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + "Is not old enough, don't let HER in.");
+        } else {
+            console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + "Is not old enough, don't let HIM in.");
+        }
+    }
+}
+}
+nameAgeGender();
+console.log("=======");
+
+//#4 up to 100
+var num = 0
+    for( var i = 0; i < 101; i++){
+      if( i % 2 == 0){
+        num++
+        console.log(num + " it is even");
+    } else {
+        console.log(num + " it is odd");
+    }
+}
+
+
+
+
+
+
+
+ /* for ( var i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad[i].length; i++ ) {
+  if (peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18) {
+    if (peopleWhoWantToSeeMadMaxFuryRoad[i].gender === "female"){
+         console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + "is Old Enough, please let HER in ");
+    }  else {
+         console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + "is Old Enough, please let HIM in "); 
+    }
+  }  else {
+      if(peopleWhoWantToSeeMadMaxFuryRoad[i].gender === "female"){
+          console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + "Is not old enough, do not let Her in");
+      } else {
+          console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + "is not old enough, do not let HIM in");
+      }
+  }
+} */
