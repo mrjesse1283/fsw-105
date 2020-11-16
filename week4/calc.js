bf4d28dd408a84fd36b010bb518a3f276e8992c8
jesse hernandez <mrjesse1283>
2020-11-15 19:31:53 -0700

    project2 week4 calc

--- week4/calc.js
+++ week4/calc.js
@@ -1,23 +1,40 @@
-var readlineSync = require('readline-sync');
-
-var firstNumber = readlineSync.questionInt("please enter your first numeber.");
+var readlineSync = require('readline-sync');//required to run module. 
+//necessrry variables to be able to use calculator.
+var firstNumber = readlineSync.questionInt("Please enter your first number.");
 var secondNumber = readlineSync.questionInt("Please enter your second number.");
 var enteredOperator = readlineSync.question("What operation yould you like to perform? add/sub/mul/div: ");
 
+var resultAdd = AddTwoNumnber(firstNumber , secondNumber)
 function AddTwoNumnber(num1 , num2){
-    return +num1 + +num2;
+    return (num1 + num2);
 }
 
-var resultAdd = AddTwoNumnber(firstNumber , secondNumber)
+var resultSub = subTwoNums(firstNumber, secondNumber)
+function subTwoNums(num1 , num2){
+    return (num1 - num2);
+}
+
+var resultMul = multiplyTwoNumber(firstNumber,secondNumber)
+function multiplyTwoNumber(num1 , num2){
+    return (num1 * num2);
+}
+var resultDiv = divTwoNums(firstNumber, secondNumber)
+function divTwoNums(num1 , num2){
+    return (num1 / num2);
+}
+//var resultAdd = AddTwoNumnber(firstNumber , secondNumber)
     if(enteredOperator === "add"){
         AddTwoNumnber(firstNumber, secondNumber);
         console.log(firstNumber + " + " + secondNumber + " == " +resultAdd) 
 }   else if(enteredOperator === "sub"){
-
+        subTwoNums(firstNumber, secondNumber);
+        console.log(firstNumber + " - " + secondNumber + " == " +resultSub) 
 }   else if(enteredOperator === "mul"){
-
+        multiplyTwoNumber(firstNumber, secondNumber);
+        console.log(firstNumber + " * " + secondNumber + " == " +resultMul);
 }   else if(enteredOperator === "div"){
-
-}   else{
+        divTwoNums(firstNumber, secondNumber);
+        console.log(firstNumber + " / " + secondNumber + " == " +resultDiv);
+}   else{ 
     console.log("cannot recognize the operation provided, PLEASE TRY AGAIN!!");
 }
