9003b06e0dc02f655c79e2fe5302b49a0f76fc62
jesse hernandez <mrjesse1283>
2020-11-22 23:30:22 -0700

    ES6.2

--- week5/ES6 2/es6.js
+++ week5/ES6 2/es6.js
@@ -1,24 +1,23 @@
 //1
-/* var name = "john";
-var age = 101
+const manName = "John";
+const age = 101;
 
 function runForLoop(pets){
-    var petObjects = [];
-    for(var i = 0, i < pets.length; i++){
-        var pet = {type: pets[i]}
-        name;
-        if{pets[i] === "cat"} {
-            name = "fluffy"
-        } else {
-            name = "spot"
-        }
-        console.log("pet name: " + name)
-        return pet.name = name
-        petObjects.push(pets)
+    const petObjects = [];
+    for(let i = 0; i < pets.length; i++) {
+        let pet = {type: pets[i]}
+        //let name;
+        if(pets[i] === "cat") {
+            name = "fluffy";
+        } else { name = "spot";}
+          console.log("pet name: ",  name)
+          pet.name = name;
+          petObjects.push(pets);
     }
-    console.log("man name: " + name);
+    console.log("man name: " + manName);
+    return petObjects;
 }
-runForLoop(["cat", "dog"]);  */
+runForLoop(["cat", "dog"]);  
 
 //2
 const carrots =["bright orange", "ripe", "rotten"];
@@ -31,3 +30,60 @@ mapVegetables = arr => {
 console.log(mapVegetables([carrots]));
 
 //3
+const people = [
+    {
+        name: "Princess Peach",
+        friendly: false
+    },
+    {
+        name: "Luigi",
+        friendly: true
+    },
+    {
+        name: "Mario",
+        friendly: true
+    },
+    {
+        name: "Bowser",
+        friendly: false
+    }
+]
+const filterForFriendly = (arr) => 
+    arr.filter((person) => person.friendly);
+
+console.log(filterForFriendly(people));
+
+//4
+var doMathSum = (a, b) => (a + b);
+
+var produceProduct = (a, b) => (a * b);
+
+//5 
+function printString (firstName = "Jane", lastName ="Doe", age = "100"){
+    //return "Hi "  + firstName + lastName + ", how does it feel to be " + age;
+//6
+    return `Hi ${firstName} ${lastName} , how does it feel to be ${age}?`
+}
+console.log(printString());
+
+//7
+const animals = [
+    {type:"dog", name:"theodore"},
+    {type:"cat", name:"whiskers"},
+    {type:"pig", name:"piglet"},
+    {type:"dog", name:"sparky"},
+];
+filterForDogs = arr => arr.filter(animal => (animal.type === "dog") ? true: false);
+
+console.log(filterForDogs([
+    {type:"dog", name:"theodore"},
+    {type:"cat", name:"whiskers"},
+    {type:"pig", name:"piglet"},
+    {type:"dog", name:"sparky"},
+]));
+
+//8
+function templateString(location, name) {
+    return (`Hi ${name}, \n Thank you for visiting, ${location}. \n I hope you enjoyed your stay, you dont have to go home, but you gotta get the hell outta here!! `)
+}
+console.log(templateString("Yo mommas house", "Jesse"));
