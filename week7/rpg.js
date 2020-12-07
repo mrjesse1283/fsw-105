3e4e1c65e167a1f2e9a4f332bf4deb4bb2c6981c
jesse hernandez <mrjesse1283>
2020-12-06 19:00:39 -0700

    rpg commit

--- week7/rpg.js
+++ week7/rpg.js
@@ -2,33 +2,82 @@
 
 var readlineSync = require('readline-sync');
 
-const playerName = readlineSync.question("Sorry you fell into my yard, what is your Name? ");
+const name = readlineSync.question("HAHA you fell into my yard, what is your Name? ");
 
-const welcomeMessage = `${playerName}, to make it out of here alive you will need to choose wiseley. `;
-console.log(welcomeMessage);
+readlineSync.question(`${name}, to make it out of here alive you will need to choose wiseley. Please Press Enter.  `);
+/* console.log(welcomeMessage); */
+const badGuys = ["Monster", "Nightmare", "BlankMan", "Smasher"];
 
-let isAlive = true;
-let foundKey = false;
+const treasure = ["shield", "food", "shelter", "water"];
+var prize = [];
 
-while (isAlive == true)
-{
-    const menuId = readlineSync.keyIn(` Press 1: \t to put hand in the hole \n Press 2:\t to find the key \n Press 3:\t to open the door `,{limit: `$<1-3>`});
-    if (menuId ==1){
-        console.log(`\tSo sorry ${playerName}, The game is OVER!!`);
-        isAlive = false;
+let userHealth = 40;
+
+const options = ["Walk", "Exit", "Print"];
+
+let pickUp = treasure[Math.floor(Math.random() * treasure.length)];
+
+
+function game(){
+    const attackPower = Math.floor(Math.random() * (5 - 4 + 3) + 5);
+    const badGuy = badGuys[Math.floor(Math.random() * badGuys.length)];
+    let badGuysHealth = 40;
+    const badGuysPower = Math.floor(Math.random() * (5 - 3 + 2) + 5);
+
+    const index = readlineSync.keyInSelect(options, "What will be your destiny? ");
+
+    if(options[index] == "Exit") {
+        return userHealth == 0;
+    }else if (options[index] == "Print") {
+        console.log(name + ":\n" + userHealth + ":Prize" + pickUp);
+    }else if (options[index] == "Walk"){
+        let key = Math.random();
+        if(key <= '.3') {
+            console.log("walking....");
+        }else if(key >= '.3') {
+            console.log(badGuy + "Is in your presence!");
+
+            while(badGuysHealth > 0 && userHealth > 0) {
+
+                const user = readlineSync.question("What do you want to do? Enter 'r' to run or 'a' to attack: ");
+
+                switch (user){
+                    case 'r':
+                        const run = Math.random();
+                        if(run < '.5') {
+                            console.log("Before you flee, " + badGuy + " attacks you with, " + badGuysPower );
+                        }else {
+                            console.log("You Ran Away");
+                            //break;
+                        }break;
+
+                    case 'a':
+                        badGuysHealth -= attackPower;
+                        console.log("You attacked " + badGuy + "with " + attackPower + " Power. ");
+                        
+                        userHealth -= badGuysPower;
+                        console.log("The villian just attacked you with " + badGuysPower + " power.");
+
+                        if(badGuysHealth <= 0) {
+                            console.log("You destroyed " + badGuy + ".\n" + "\t" + badGuy + " dropped " + pickUp );
+                            let loot = Math.random();
+                            if(loot <= '.3'){
+                                prize.push(pickUp);
+                            }
+                            }else if(userHealth <= 0) {
+                                console.log(badGuy + "has devastated you, You are dead!");
+                            }
+                }
+            }
+        }
     }
-    else if (menuId == 2 && foundKey == true) {
-        console.log(` Wow ${playerName}, it seems you have found the key, move on to choice 3. `);
-    }
-    else if (menuId == 2 && foundKey == false) {
-        console.log(` I am impressed ${playerName}, you found the key successfully! Access GRANTED to choice 3.`);
-        foundKey = true;
-    }
-    else if (menuId == 3 && foundKey == false) {
-        console.log(` Not surprised ${playerName}, you have not found the key! Please proceed to choice 2.`);
-    }
-    else if (menuId == 3 && foundKey == true) {
-        console.log(` Your greatness was shown today ${playerName}, you have found the key and opened the door, You win now go home.`);
-        isAlive = false;
-    }
+}
+
+while(userHealth > 0) {
+    userRestore = function() {
+        userActive = true;
+        userHealth = 40;
+    };
+    userRestore();
+    game();
 }
